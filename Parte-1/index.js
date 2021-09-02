3264a4069e0f5b935b63e6344824698af9b3c510
EC2 Default User <ec2-user@ip-172-31-93-188.ec2.internal>
2021-09-02 21:57:40 +0000

    Primeiro commit

---
+++ Parte-1/index.js
@@ -0,0 +1,15 @@
+# Desafio DevOps Datenworks :: Thiago Paz #
+
+const express = require('express')
+
+const app = express()
+
+const port = 3000
+
+app.listen(port, () => {
+	console.log(`app listenning at http://localhost:${port}`)
+});
+
+app.get('/api', function (req, res) {
+	res.send('Hello Datenworks!')
+});
